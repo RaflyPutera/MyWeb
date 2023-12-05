5b506d1bf989d836aa4b3ec08d028cfcbbd387d4
Rafly <Rafly.S.putera@gmail.com>
2023-12-05 20:37:18 +0800

    initialize new empty web application

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>Hello</h1>
+    <span>This is somehwere to begin.</span>
+    </>
+  )
+}
+
+export default App
